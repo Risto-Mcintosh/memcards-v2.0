8743509ecdf3ab08f7af9eb71cde04b6018f9177
Risto McIntosh <Risto.mcintosh@gmail.com>
2019-07-04 19:11:55 -0500

    add netlify functions

---
+++ netlifyFunctions/unsplash/unsplash.js
@@ -0,0 +1,34 @@
+const Unsplash = require("unsplash-js").default;
+const toJson = require("unsplash-js").toJson;
+const fetch = require("node-fetch");
+global.fetch = fetch;
+
+const { UNSPLASH_API_ID, UNSPLASH_API_SECRET } = process.env;
+const unsplash = new Unsplash({
+  applicationId: UNSPLASH_API_ID,
+  secret: UNSPLASH_API_SECRET
+});
+
+const getImages = function(searchTerm, page) {
+  return unsplash.search
+    .photos(searchTerm, page, 10, "landscape")
+    .then(toJson)
+    .then(json => json)
+    .catch(e => console.log(e));
+};
+
+exports.handler = async event => {
+  try {
+    const images = await getImages(
+      event.queryStringParameters.searchTerm,
+      event.queryStringParameters.page
+    );
+
+    return {
+      statusCode: 200,
+      body: images
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+};
