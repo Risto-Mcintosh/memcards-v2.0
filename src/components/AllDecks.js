5d01fed80e0c501f40c23969480bd6144c23f125
Risto McIntosh <Risto.mcintosh@gmail.com>
2019-07-28 22:05:05 -0500

    update allDecks

--- src/components/AllDecks.js
+++ src/components/AllDecks.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { Container, ListGroup } from "react-bootstrap";
-import { Delete } from "styled-icons/material/Delete";
-import { Link } from "react-router-dom";
-import AddNewButtons from "./AddNewButtons";
+import React from 'react'
+import { Container, ListGroup } from 'react-bootstrap'
+import { Delete } from 'styled-icons/material/Delete'
+import { Link } from 'react-router-dom'
+import AddNewButtons from './AddNewButtons'
 
 export default function AllDecks({
   decks,
@@ -21,27 +21,27 @@ export default function AllDecks({
               to={`/deck/${deck.name}`}
               key={deck.name}
               onClick={async e => {
-                e.stopPropagation();
-                await setCurrentDeck(deck);
-                getCard("random");
+                e.stopPropagation()
+                await setCurrentDeck(deck)
+                getCard('random')
               }}
               action
               className={`d-flex justify-content-between ${
-                deck.data.length <= 0 ? "text-muted" : null
+                deck.data.length <= 0 ? 'text-muted' : null
               }`}
               style={{
-                pointerEvents: `${deck.data.length <= 0 ? "none" : "auto"} `
+                pointerEvents: `${deck.data.length <= 0 ? 'none' : 'auto'}`
               }}
             >
               <div className="d-flex align-items-center">
                 {deck.editable && toggle.toggleDelete && (
                   <Delete
                     className="text-danger"
-                    style={{ width: "30px", pointerEvents: "auto" }}
+                    style={{ width: '30px', pointerEvents: 'auto' }}
                     onClick={e => {
-                      e.stopPropagation();
-                      e.preventDefault();
-                      deleteDeck(deck.name);
+                      e.stopPropagation()
+                      e.preventDefault()
+                      deleteDeck(deck.name)
                     }}
                   />
                 )}
@@ -49,12 +49,12 @@ export default function AllDecks({
               </div>
               <div className=" font-weight-bold">{deck.data.length}</div>
             </ListGroup.Item>
-          );
+          )
         })}
       </ListGroup>
       <AddNewButtons
         userMadeDecks={decks.some(deck => deck.editable === true)}
       />
     </Container>
-  );
+  )
 }
