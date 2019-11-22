ba013fd2af311039ee422d396ca627676241e8d4
Risto Mcintosh <risto.mcintosh@gmail.com>
2019-11-21 23:42:04 -0600

    add deck schema

--- server/src/main.controller.ts
+++ server/src/main.controller.ts
@@ -2,14 +2,14 @@ import { Application } from 'express';
 import MongoService from './services/mongo.service';
 
 export default class Controller {
-  private mongoService: MongoService;
+  private dataService: MongoService;
 
   constructor(private app: Application) {
-    this.mongoService = new MongoService();
+    this.dataService = new MongoService();
     this.routes();
   }
 
   public routes(): void {
-    this.app.route('/').get(this.mongoService.sayHi);
+    this.app.route('/').get(this.dataService.sayHi);
   }
 }
--- server/src/models/flashcard.model.ts
+++ server/src/models/flashcard.model.ts
@@ -1,5 +1,19 @@
 import mongoose from 'mongoose';
 
 const FlashCardSchema = new mongoose.Schema({
-  name: String
+  front: String,
+  back: String,
+  image: {
+    src: String,
+    alt: String,
+    thumb: String
+  }
 });
+
+const DeckSchema = new mongoose.Schema({
+  name: String,
+  editable: Boolean,
+  data: [FlashCardSchema]
+});
+
+export const Deck = mongoose.model('Deck', DeckSchema);
